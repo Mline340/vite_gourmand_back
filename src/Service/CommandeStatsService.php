658696c88ba06159c98ff73e900318e3f8b29ac3
Emeline Brun <emeline_3010@hotmail.fr>
2026-02-16 12:48:00 +0100

    ajout filtre date stat + mise en page avis

--- src/Service/CommandeStatsService.php
+++ src/Service/CommandeStatsService.php
@@ -27,48 +27,68 @@ class CommandeStatsService
         ->getResult();
     
     // Compte les commandes par menu
-    $stats = [];
+     $collection->deleteMany([]);
+    
     foreach ($commandes as $commande) {
-        
-        $prixCommande = (float) $commande->getPrixMenu();
-        
-        foreach ($commande->getMenus() as $menu) {
-            $menuId = $menu->getId();
+        if ($commande->getStatut() && $commande->getStatut()->value === 'Terminé') {
+            $prixCommande = (float) $commande->getPrixMenu();
+            $dateTerminee = $commande->getModifiedAt(); 
             
-            if (!isset($stats[$menuId])) {
-                $stats[$menuId] = [
-                    'menu_id' => $menuId,
+             // Si pas de date, skip
+                if (!$dateTerminee) continue;
+                
+            foreach ($commande->getMenus() as $menu) {
+                $collection->insertOne([
+                    'menu_id' => $menu->getId(),
                     'menu_titre' => $menu->getTitre(),
-                    'total_commandes' => 0,
                     'prix_par_personne' => (float) $menu->getPrixParPersonne(),
-                    'chiffre_affaires' => 0.0
-                ];
+                    'prix_commande' => $prixCommande,
+                    'date_terminee' => new UTCDateTime($dateTerminee->getTimestamp() * 1000),
+                ]);
             }
-             if ($commande->getStatut() && $commande->getStatut()->value === 'Terminé') {
-            $stats[$menuId]['total_commandes']++;
-            $stats[$menuId]['chiffre_affaires'] = (float) ($stats[$menuId]['chiffre_affaires'] + $prixCommande);
-            }
-        }
-    }
-    
-    // Vide et réinsère dans MongoDB
-    $collection->deleteMany([]);
-    
-    foreach ($stats as $stat) {
-        $collection->insertOne([
-            'menu_id' => $stat['menu_id'],
-            'menu_titre' => $stat['menu_titre'],
-            'total_commandes' => $stat['total_commandes'],
-            'prix_par_personne' => (float) $stat['prix_par_personne'],
-            'chiffre_affaires' => (float) $stat['chiffre_affaires'] 
             
-        ]);
         }
     }
+    
+}
 
-    public function getStats()
-    {
-        $collection = $this->mongoService->getCollection('commande_stats');
-        return $collection->find([], ['sort' => ['total_commandes' => -1]])->toArray();
+    public function getStats(?string $menuId = null, ?\DateTime $dateDebut = null, ?\DateTime $dateFin = null)
+{
+    $collection = $this->mongoService->getCollection('commande_stats');
+    
+    $filter = [];
+    
+    if ($menuId) {
+        $filter['menu_id'] = (int) $menuId;
     }
+    
+    if ($dateDebut || $dateFin) {
+        $filter['date_terminee'] = [];
+        if ($dateDebut) {
+            $filter['date_terminee']['$gte'] = new UTCDateTime($dateDebut->getTimestamp() * 1000);
+        }
+        if ($dateFin) {
+            $filter['date_terminee']['$lte'] = new UTCDateTime($dateFin->modify('+1 day')->getTimestamp() * 1000);
+        }
+    }
+    
+    // Pipeline sans $match si filtre vide
+    $pipeline = [];
+    
+    if (!empty($filter)) {
+        $pipeline[] = ['$match' => $filter];
+    }
+    
+    $pipeline[] = ['$group' => [
+        '_id' => '$menu_id',
+        'menu_titre' => ['$first' => '$menu_titre'],
+        'total_commandes' => ['$sum' => 1],
+        'prix_par_personne' => ['$first' => '$prix_par_personne'],
+        'chiffre_affaires' => ['$sum' => '$prix_commande']
+    ]];
+    
+    $pipeline[] = ['$sort' => ['total_commandes' => -1]];
+    
+    return $collection->aggregate($pipeline)->toArray();
+}
 }
--- src/State/CommandeStatsProvider.php
+++ src/State/CommandeStatsProvider.php
@@ -5,23 +5,38 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Service\CommandeStatsService;
 use App\ApiResource\CommandeStats;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class CommandeStatsProvider implements ProviderInterface
 {
-    public function __construct(private CommandeStatsService $statsService) {}
+    public function __construct(
+        private CommandeStatsService $statsService,
+        private RequestStack $requestStack
+    ) {}
 
-   public function provide(Operation $operation, array $uriVariables = [], array $context = []): array
-{
-    $stats = $this->statsService->getStats();
-    
-    return array_map(function($item) {
-        $stat = new CommandeStats();
-        $stat->menuId = (string)$item->_id;
-        $stat->menuLibelle = $item->menu_titre;
-        $stat->nombreCommandes = $item->total_commandes;
-        $stat->chiffreAffaires = $item->chiffre_affaires ?? 0;
-        $stat->prixParPersonne = $item->prix_par_personne ?? 0;
-        return $stat;
-    }, $stats);
-}
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): array
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        
+        // Récupérer les filtres depuis l'URL
+        $menuId = $request?->query->get('menuId');
+        $dateDebut = $request?->query->get('dateDebut');
+        $dateFin = $request?->query->get('dateFin');
+        
+        // Convertir les dates
+        $dateDebutObj = $dateDebut ? \DateTime::createFromFormat('Y-m-d', $dateDebut) : null;
+        $dateFinObj = $dateFin ? \DateTime::createFromFormat('Y-m-d', $dateFin) : null;
+        
+        $stats = $this->statsService->getStats($menuId, $dateDebutObj, $dateFinObj);
+        
+        return array_map(function($item) {
+            $stat = new CommandeStats();
+            $stat->menuId = (string)$item->_id;
+            $stat->menuLibelle = $item->menu_titre;
+            $stat->nombreCommandes = $item->total_commandes;
+            $stat->chiffreAffaires = $item->chiffre_affaires ?? 0;
+            $stat->prixParPersonne = $item->prix_par_personne ?? 0;
+            return $stat;
+        }, $stats);
+    }
 }
